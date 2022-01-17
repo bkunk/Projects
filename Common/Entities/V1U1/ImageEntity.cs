9e4741a913b3e000275b4d95a37b12dbd06efd83
bkunk <83511927+bkunk@users.noreply.github.com>
2022-01-16 20:04:31 -0600

    Add files via upload

---
+++ Common/Entities/V1U1/ImageEntity.cs
@@ -0,0 +1,49 @@
+﻿using CloudStorage.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CloudStorage.Entities.V1U1
+{
+    [ApiVersion("1.1")]
+    public class ImageEntity
+    {
+
+        public string UserName { get; internal set; }
+
+        public string Id { get; internal set; }
+
+        [MinLength(3)]
+        public string Name { get; set; }
+
+        public string UploadUrl { get; internal set; }
+
+        [Required]
+        [MinLength(5)]
+        public string Description { get; set; }
+
+        public ImageTableEntity ToTableEntity()
+        {
+            return new ImageTableEntity()
+            {
+                UserName = this.UserName,
+                Name = this.Name,
+                Description = this.Description
+            };
+        }
+
+        public static ImageEntity FromTableEntity(ImageTableEntity tableEntity)
+        {
+            return new ImageEntity()
+            {
+                UserName = tableEntity.UserName,
+                Id = tableEntity.Id,
+                Name = tableEntity.Name,
+                Description = tableEntity.Description
+    };
+        }
+    }
+}
---
+++ Common/Models/ImageTableEntity.cs
@@ -0,0 +1,44 @@
+﻿using CloudStorage.Entities;
+using Microsoft.Azure.Cosmos.Table;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CloudStorage.Models
+{
+    /// <summary>
+    /// This class is persisted to a database. It's a "model."
+    /// </summary>
+    public class ImageTableEntity : TableEntity
+    {
+        public ImageTableEntity(string userName, string name)
+        {
+            this.UserName = userName;
+            this.Name = name;
+        }
+
+        public ImageTableEntity()
+        {
+
+        }
+
+        public string UserName
+        {
+            get { return this.PartitionKey; }
+            set { this.PartitionKey = value; }
+        }
+
+        public string Id {
+            get { return this.RowKey; }
+            set { this.RowKey = value; }
+        }
+
+        public string Name { get; set; }
+
+        public bool UploadComplete { get; set; }
+
+        public string Description { get; set; }
+
+    }
+}
